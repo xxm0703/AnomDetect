8c36761542923792e6600f3d7850e7527208c3d3
Samuil Georgiev <sami.georgiev@gmail.com>
2018-09-11 17:34:58 +0300

    signIN

--- WebApp/resources/views/welcome.blade.php
+++ WebApp/resources/views/welcome.blade.php
@@ -1,18 +1,15 @@
 <!doctype html>
-<<<<<<< HEAD
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/site/docs/4.1/examples/sign-in/signin.css">
 </head>
-    
-=======
-<link rel="stylesheet" type="text/css" href="/css/style.css">
->>>>>>> 8cc5ce594e7fa9726a245f5b6b8d62b50bbb0382
 <body>
-    <div class="center">
-        <button class="block">Register</button>
-        <button class="block">Login In</button>
-    </div>
+    <form class="form-signin">
+        <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
 </body>
-
 </html>
