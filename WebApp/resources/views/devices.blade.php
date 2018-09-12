c298a8eafa97e4b1a6025ec4bdadc373ea7e30f5
Georgi Lyubenov <joro2404@abv.bg>
2018-09-12 15:17:42 +0300

    device page

--- WebApp/resources/views/devices.blade.php
+++ WebApp/resources/views/devices.blade.php
@@ -22,14 +22,5 @@
         </div>
       </header>
   	</div>
-  	<div class="joro">	
-		<main role="main" class="inner cover">
-        	<h1 class="cover">Welcome to Anomaly Detection</h1>
-        	<p class="txcol">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        	<p class="lead">
-          	<a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        	</p>
-      </main>
-  	</div>
 </body>
 </html>
