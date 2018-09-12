2673c32ad796942788470c84bb863eb6232cd3ca
Martin Jordanov <xxm0703@abv.bg>
2018-09-12 11:04:02 +0300

    Login accept

--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('main');
-});
+Route::post('/login/submit', LoginController@validate);
