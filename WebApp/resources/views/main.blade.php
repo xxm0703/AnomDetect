7bae6162bf562421f5ce3410ad65a57fd8d8910e
Georgi Lyubenov <joro2404@abv.bg>
2018-09-12 10:53:11 +0300

    home page re

--- WebApp/resources/views/main.blade.php
+++ WebApp/resources/views/main.blade.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <html lang="en">
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/site/docs/4.1/examples/cover/cover.css">
@@ -11,20 +15,21 @@
         <div class="inner">
           <h3 class="masthead-brand">AnomDetect</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">Your Profile</a>
+            <a class="nav-link active" href="#">Home</a>
             <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="#">Contact</a>
           </nav>
         </div>
       </header>
   	</div>
-	<main role="main" class="inner cover">
-        <h1 class="cover-heading">Welcome to Anomaly Detection</h1>
-        <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
+  	<div class="joro">
+		<main role="main" class="inner cover">
+        	<h1 class="cover-heading">Welcome to Anomaly Detection</h1>
+        	<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        	<p class="lead">
+          	<a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        	</p>
       </main>
+  	</div>
 </body>
 </html>
