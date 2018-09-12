293ec8d1d985041394f1f3da8b587b5370724c29
Georgi Lyubenov <joro2404@abv.bg>
2018-09-12 11:05:56 +0300

    home page ve

--- WebApp/resources/views/welcome.blade.php
+++ WebApp/resources/views/welcome.blade.php
@@ -5,11 +5,11 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap/site/docs/4.1/examples/sign-in/signin.css">
 </head>
 <body>
-    <form class="form-signin">
+    <form class="form-signin" action="/login/submit">
         <h1 class="h3 mb-3 font-weight-normal">Sign in</h1>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" >Sign in</button>
     </form>
 </body>
 </html>
