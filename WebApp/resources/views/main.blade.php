b6309ee49a446dfd6601913da9dd8b3a0018beb5
Georgi Lyubenov <joro2404@abv.bg>
2018-09-12 10:09:12 +0300

    home page

---
+++ WebApp/resources/views/main.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/site/docs/4.1/examples/cover/cover.css">
+</head>
+<body class="text-center">
+	<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">AnomDetect</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Your Profile</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">About Us</a>
+          </nav>
+        </div>
+      </header>
+  	</div>
+	<main role="main" class="inner cover">
+        <h1 class="cover-heading">Welcome to Anomaly Detection</h1>
+        <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+</body>
+</html>
